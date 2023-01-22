c6db6f517d11e2b4a5117a0aaacaec66976c4784
ndaemy <yuyaebean@gmail.com>
2023-01-23 04:09:09 +0900

    feat: Application resource 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ApplicationsModule } from './api/applications/applications.module';
 
 @Module({
-  imports: [],
+  imports: [ApplicationsModule],
   controllers: [],
   providers: [],
 })
