8a7a7c0acf1ef277405d745e110dbe9d6e3ccf15
ndaemy <yuyaebean@gmail.com>
2023-01-24 04:29:24 +0900

    setting: TypeORM 으로 DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ApplicationsModule } from './api/applications/applications.module';
+import databaseConfig from './config/database.config';
 import { validationSchema } from './config/validationSchema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      load: [databaseConfig],
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      synchronize: process.env.NODE_ENV === 'development',
     }),
     ApplicationsModule,
   ],
--- src/config/validationSchema.ts
+++ src/config/validationSchema.ts
@@ -3,4 +3,9 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   NODE_ENV: Joi.string().allow('development', 'production').default('development'),
   PORT: Joi.number().default(3000),
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_PORT: Joi.number().port().default(3306),
+  DATABASE_USERNAME: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
 });
