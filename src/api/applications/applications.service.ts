3846bb675c01e862d176bf4d14a49e37d78aae51
ndaemy <yuyaebean@gmail.com>
2023-02-14 03:09:49 +0900

    fix: Conflict 발생 시 500 에러가 터지던 것을 409 에러로 변경

--- src/api/applications/applications.service.ts
+++ src/api/applications/applications.service.ts
@@ -1,5 +1,9 @@
 import { PutObjectCommand } from '@aws-sdk/client-s3';
-import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -16,6 +20,31 @@ export class ApplicationsService {
 
   async create(createApplicationDto: CreateApplicationDto) {
     const { personalInfo, applicationInfo } = createApplicationDto;
+
+    const conflictErrors: string[] = [];
+    const [samePhoneUser] = await this.applicationRepository.findBy({
+      phone: personalInfo.phone,
+    });
+    const [sameEmailUser] = await this.applicationRepository.findBy({
+      email: personalInfo.email,
+    });
+    const [sameSidUser] = await this.applicationRepository.findBy({
+      sid: personalInfo.sid,
+    });
+
+    if (samePhoneUser) {
+      conflictErrors.push(`phone ${personalInfo.phone} is already exist`);
+    }
+    if (sameEmailUser) {
+      conflictErrors.push(`email ${personalInfo.email} is already exist`);
+    }
+    if (sameSidUser) {
+      conflictErrors.push(`sid ${personalInfo.sid} is already exist`);
+    }
+
+    if (conflictErrors.length > 0) {
+      throw new ConflictException(conflictErrors);
+    }
 
     // 디지몬 파트가 아닌데 자기소개서가 없으면 400 에러
     if (personalInfo.part !== 'design') {
