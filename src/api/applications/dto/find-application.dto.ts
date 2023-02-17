372ea5f728f83a3c449c6a45978c69366e485d3d
ChoiWonYu <phillip07290@naver.com>
2023-02-17 14:45:12 +0900

    refactor: 안 쓰는 줄 삭제

--- src/api/applications/dto/find-application.dto.ts
+++ src/api/applications/dto/find-application.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsEnum,
-  IsIn,
-  IsNumberString,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsIn, IsNumberString, IsOptional, IsString } from 'class-validator';
 
 export class PagenationOptions {
   @IsNumberString()
